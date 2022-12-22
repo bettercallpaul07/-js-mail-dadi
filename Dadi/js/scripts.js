292350cbd9962af03aede94db38341a046c1d8ff
Paolo Rapuano <bettercallpaul07@gmail.com>
2022-12-22 13:56:07 +0100

    Dadi Complete

---
+++ Dadi/js/scripts.js
@@ -0,0 +1,26 @@
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+//Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+//Prendo un numero tra 0 e un 1 (estremo superiore escluso) con math random lo moltiplico per 6, gli aggiungo piu 1 e prende l'intero inferiore.
+
+let computer = Math.floor(Math.random() * 6 + 1);
+let player = Math.floor(Math.random() * 6 + 1);
+
+console.log("Numero giocatore umano:", player)
+console.log("Numero computer:", computer)
+
+
+if (player > computer) {
+    console.log("Ha vinto il giocatore umano!")
+
+}
+
+else if (player < computer) {
+    console.log ("Ha vinto il computer!")
+}
+
+else {
+    console.log("Pareggio!")
+}
+
